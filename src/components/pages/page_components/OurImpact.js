bc551748f2243fe1f263fb1c7bcc4efd8b65e9f8
RD-Singh <rdmalhans.02@gmail.com>
2023-01-18 23:17:44 -0800

    Updated Initiatives page to include upcoming events

--- src/components/pages/page_components/OurImpact.js
+++ src/components/pages/page_components/OurImpact.js
@@ -13,25 +13,33 @@ export default class OurImpact extends Component {
 
     render() {
         return (
-            <div className="impact-container">
-                <div className="impact-text">
-                    <GiBullseye className="bullseye-icon"/>
-                    <span className="impact-title">Our Impact</span>
-                    <span className="first-event">Results of our first event on July 24th, 2021</span>
-                </div>
-                <div className="impact-stats">
-                    <div data-aos="fade-right" data-aos-once="true" className="stat-card-container-one">
-                        <FaMoneyBill className="stat-icon"/>
-                        <p className="stat-desc">Collected over</p>
-                        <p className="countup-stat-text"><CountUp className="countup-stat" end={15000} prefix="$" duration={4.5} useEasing="ease" /><br/>worth of electronics</p>
+            <div>
+                <div className="impact-container">
+                    <div className="impact-text">
+                        <span className="impact-title">Our next event will be held on <br/>February 21st</span>
                     </div>
-                    <div data-aos="fade-left" data-aos-once="true" className="stat-card-container-two">
-                        <FaRecycle className="stat-icon"/>
-                        <p className="stat-desc">Recycled</p>
-                        <p className="countup-stat-text"><CountUp className="countup-stat" end={250} duration={4.5} suffix="+" useEasing="ease"/><br/>devices</p>
+                </div>
+                <div className="impact-container">
+                    <div className="impact-text">
+                        <GiBullseye className="bullseye-icon"/>
+                        <span className="impact-title">Our Impact</span>
+                        <span className="first-event">Results of our of our events in 2021</span>
+                    </div>
+                    <div className="impact-stats">
+                        <div data-aos="fade-right" data-aos-once="true" className="stat-card-container-one">
+                            <FaMoneyBill className="stat-icon"/>
+                            <p className="stat-desc">Collected over</p>
+                            <p className="countup-stat-text"><CountUp className="countup-stat" end={15000} prefix="$" duration={4.5} useEasing="ease" /><br/>worth of electronics</p>
+                        </div>
+                        <div data-aos="fade-left" data-aos-once="true" className="stat-card-container-two">
+                            <FaRecycle className="stat-icon"/>
+                            <p className="stat-desc">Recycled</p>
+                            <p className="countup-stat-text"><CountUp className="countup-stat" end={700} duration={4.5} suffix="+" useEasing="ease"/><br/>kgs of waste</p>
+                        </div>
                     </div>
                 </div>
             </div>
+            
         )
     }
 }
