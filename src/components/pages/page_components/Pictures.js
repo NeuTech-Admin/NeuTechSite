67cbef5f40df24e0d512c42cf7278e6104b490a7
AD-SINGH-M <admalhans.06@gmail.com>
2023-04-02 18:53:03 -0700

    fixed gallery page and a bit of navbar

--- src/components/pages/page_components/Pictures.js
+++ src/components/pages/page_components/Pictures.js
@@ -1,4 +1,3 @@
-import Aos from 'aos'
 import React, { Component } from 'react'
 import './Pictures.css'
 
@@ -8,32 +7,75 @@ class Pictures extends Component {
         super(props)
 
         this.state = {
-            images: 'images'
+            images: 'images',
         }
     }
-
+    
 
     componentDidMount() {
-        Aos.init({duration: 1250})
         setTimeout(() => {
             this.setState({
                 images: 'images visible',
             })
-        }, 100);
+        }, 100); 
+        window.addEventListener('load', e => {
+            console.log('load');
+        });
+        
     }
+
+    data = [
+        {
+            src: "ERA Truck.jpg",
+            alt: "ERA Truck"
+        },
+        {
+            src: "Arduino.jpg",
+            alt: "Arduino"
+        },
+        {
+            src: "Whiteboard.jpg",
+            alt: "Whiteboard"
+        },
+        {
+            src: "Desktops.jpg",
+            alt: "Desktops"
+        },
+        {
+            src: "teaching.jpg",
+            alt: "Teaching"
+        },
+        {
+            src: "Laptop.jpg",
+            alt: "Laptop"
+        },
+        {
+            src: "electronics.jpg",
+            alt: "Electronics"
+        },
+    ];
 
     render() {
         return (
             <div className="pictures-container">
                 <p className="gallery-heading">Gallery</p>
                 <div className="pictures">
-                    <img className={`${this.state.images} eraTruck`} src="ERA Truck.jpg" alt="ERA Truck" />
+                    {
+                        this.data.map((item, index) => {
+                            return (
+                                <div className="pics" key={index}>
+                                    <img src={item.src} alt={item.alt} style={{width: "100%"}} />
+                                </div>
+                            )
+                        })
+                    }
+                    {/* <img className={`${this.state.images} eraTruck`} src="ERA Truck.jpg" alt="ERA Truck" />
                     <img className={`${this.state.images} arduino`} src="Arduino.jpg" alt="Arduino" />
                     <img className={`${this.state.images} desktops`} src="Desktops.jpg" alt="Desktops" />
                     <img className={`${this.state.images} whiteboard`} src="Whiteboard.jpg" alt="Whiteboard" />
                     <img className={`${this.state.images} teaching`} src="teaching.jpg" alt="Teaching" />
                     <img className={`${this.state.images} laptop`} src="Laptop.jpg" alt="Laptop" />
-                    <img className={`${this.state.images} electronics`} src="electronics.jpg" alt="Electronics" />
+                    <img className={`${this.state.images} electronics`} src="electronics.jpg" alt="Electronics" /> */}
                 </div>
             </div>
 
